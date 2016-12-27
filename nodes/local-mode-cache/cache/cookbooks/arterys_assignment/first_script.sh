5453821c700ef0e27f3b4833981e64a586521fef
vagrant <vagrant@Ubuntu-1>
2016-12-27 17:03:30 +0000

    test

--- nodes/local-mode-cache/cache/cookbooks/arterys_assignment/first_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PARENT_DIR="$(dirname `pwd`)"
-cat <<EOF > ./nodes/solo.rb
-cookbook_path "$PARENT_DIR"
-EOF
--- nodes/local-mode-cache/cache/cookbooks/arterys_assignment/metadata.rb
+++
@@ -1,5 +0,0 @@
-name             'arterys_assignment'
-maintainer       'Rafik Benmansour'
-maintainer_email 'raf_ben@hotmail.com'
-license          'All rights reserved'
-version          '0.1.0'
